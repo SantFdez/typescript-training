79c00ea2d173e4c08bdd38fbca89df38cab05e61
Santiago Fernández <sannt43@gmail.com>
2024-05-02 18:38:43 -0500

    feat: refactor text processor

--- projects/arrays/text-processor/src/index.ts
+++ projects/arrays/text-processor/src/index.ts
@@ -6,64 +6,72 @@ export type Text = {
 	width: number;
 };
 
+export function makeLines(text: string, width: number) {
+	let words = text.split(" ");
+
+	const lines: string[] = [];
+	let line = "";
+
+	for (const word of words) {
+		if (line === "") {
+			line = word;
+		} else if (line.length + word.length < width) {
+			line += ` ${word}`;
+		} else {
+			lines.push(line);
+			line = word;
+		}
+	}
+
+	lines.push(line);
+	return lines;
+}
+
+export function addSpaces(lines: string[], { align = "left", width }: Text) {
+	const aligned: string[] = [];
+
+	for (const line of lines) {
+		const remainingSpaces = width - line.length;
+		let newLine = line;
+
+		if (remainingSpaces) {
+			switch (align) {
+				case "left":
+					for (let i = 0; i < remainingSpaces; i += 1) {
+						newLine += " ";
+					}
+					break;
+
+				case "middle":
+					for (let i = 0; i < Math.ceil(remainingSpaces / 2); i += 1) {
+						newLine += " ";
+					}
+
+					for (let i = 0; i < Math.floor(remainingSpaces / 2); i += 1) {
+						newLine = " " + newLine;
+					}
+
+					break;
+
+				case "right":
+					for (let i = 0; i < remainingSpaces; i += 1) {
+						newLine = " " + newLine;
+					}
+					break;
+			}
+		}
+
+		aligned.push(newLine);
+	}
+	return aligned;
+}
+
 export function alignTexts(texts: string[], options: Text) {
 	const response: string[][] = [];
 
 	for (const text of texts) {
-		let words = text.split(" ");
-
-		const lines: string[] = [];
-		let line = "";
-
-		for (const word of words) {
-			if (line === "") {
-				line = word;
-			} else if (line.length + word.length < options.width) {
-				line += ` ${word}`;
-			} else {
-				lines.push(line);
-				line = word;
-			}
-		}
-
-		lines.push(line);
-
-		const aligned: string[] = [];
-
-		for (const line of lines) {
-			const remainingSpaces = options.width - line.length;
-			let newLine = line;
-
-			if (remainingSpaces) {
-				!options.align ? (options.align = "left") : options.align;
-				switch (options.align) {
-					case "left":
-						for (let i = 0; i < remainingSpaces; i += 1) {
-							newLine += " ";
-						}
-						break;
-
-					case "middle":
-						for (let i = 0; i < Math.ceil(remainingSpaces / 2); i += 1) {
-							newLine += " ";
-						}
-
-						for (let i = 0; i < Math.floor(remainingSpaces / 2); i += 1) {
-							newLine = " " + newLine;
-						}
-
-						break;
-
-					case "right":
-						for (let i = 0; i < remainingSpaces; i += 1) {
-							newLine = " " + newLine;
-						}
-						break;
-				}
-			}
-
-			aligned.push(newLine);
-		}
+		const lines = makeLines(text, options.width);
+		const aligned = addSpaces(lines, options);
 		response.push(aligned);
 	}
 
